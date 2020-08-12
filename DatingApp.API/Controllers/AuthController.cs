a89e26d8894c9c47ccddbc432d38b32be0ac3643
Dawid Pawulski <xxlop@eg.dk>
2020-08-12 14:36:19 +0200

    Error handling

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -77,9 +77,12 @@ namespace DatingApp.API.Controllers
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
-            return Ok(new {
+            return Ok(new
+            {
                 token = tokenHandler.WriteToken(token)
             });
+
+
         }
     }
 }
